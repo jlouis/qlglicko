38ca5bf271a363fbf3152872b2060aad408484e6
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-04-29 21:13:57 +0200

    Tournaments we should insert into the database.

---
+++ tournaments.sql
@@ -0,0 +1,7 @@
+INSERT INTO tournament (t_from, t_to) VALUES ('2012-03-06', '2012-03-13');
+INSERT INTO tournament (t_from, t_to) VALUES ('2012-03-13', '2012-03-20');
+INSERT INTO tournament (t_from, t_to) VALUES ('2012-03-20', '2012-03-27');
+INSERT INTO tournament (t_from, t_to) VALUES ('2012-03-27', '2012-04-03');
+INSERT INTO tournament (t_from, t_to) VALUES ('2012-04-03', '2012-04-10');
+INSERT INTO tournament (t_from, t_to) VALUES ('2012-04-10', '2012-04-17');
+
