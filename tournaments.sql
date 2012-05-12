f3e5427f2ef5ea4b3f9f376ab0724cb9eb6da0f2
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-05-12 19:39:35 +0200

    Improve tournament handling. Let postgres do the measurement of days.

--- tournaments.sql
+++ tournaments.sql
@@ -1,7 +1,4 @@
-INSERT INTO tournament (t_from, t_to) VALUES ('2012-03-06', '2012-03-13');
-INSERT INTO tournament (t_from, t_to) VALUES ('2012-03-13', '2012-03-20');
-INSERT INTO tournament (t_from, t_to) VALUES ('2012-03-20', '2012-03-27');
-INSERT INTO tournament (t_from, t_to) VALUES ('2012-03-27', '2012-04-03');
-INSERT INTO tournament (t_from, t_to) VALUES ('2012-04-03', '2012-04-10');
-INSERT INTO tournament (t_from, t_to) VALUES ('2012-04-10', '2012-04-17');
+INSERT INTO tournament (t_from, t_to)
+  SELECT max(t_to), max(t_to) + '7 days' :: interval FROM tournament;
+  
 
