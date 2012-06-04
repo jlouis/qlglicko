5bc52cc912128832d10b299cb0859d944b17ba51
Jesper Louis Andersen <jesper.louis.andersen@gmail.com>
2012-06-04 20:02:34 +0200

    Add the DHS2012 file to consult.

---
+++ dhs2012.erl
@@ -0,0 +1,13 @@
+%% DHS 2012 groups:
+
+[{player, "a1r_r14d", 1500, 397, 0.07},
+ {player, "hipnotic", 1500, 397, 0.07},
+ {player, "unnamedtony", 9001, 50, 0.075}].
+[{group, "A", ["evil", "fire_bot", "twister", "xeron_"]},
+ {group, "B", ["DaHanG", "pavel_", "a1r_r14d", "unnamedtony"]},
+ {group, "C", ["rapha","mAdix","jeWro","pitch"]},
+ {group, "D", ["k1llsen", "Sc00T", "STING_pl", "hipnotic"]},
+ {group, "E", ["Spart1e", "draven", "Sl1p", "ani"]},
+ {group, "F", ["dkt", "Weird", "lukelawlz", "mccormic"]},
+ {group, "G", ["Avekkk", "Pawelek85", "ins", "mdf"]},
+ {group, "H", ["Cypher", "Guard", "gda", "mousestar"]}].
